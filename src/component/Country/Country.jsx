7dc1148542d1f5b666f0b652cbe281cc4f0449f1
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-02-28 16:12:30 +0600

    added flag and population

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Country = ({country}) => {
- console.log(country)
+ console.log(country.flags.flags)
   return (
     <div>
 
+     
+     <img src={country.flags.flags.png} alt="" />
      <h4>Name :{country.name.common} </h4>
+     <p>population : {country.population.population}</p>
   
     </div>
   );
